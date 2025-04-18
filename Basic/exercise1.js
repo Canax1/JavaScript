26efcab7d0ff1c28d0e9c73a1d46718dd18bafd8
Chika Anegbu <119685383+Canax1@users.noreply.github.com>
2025-04-18 10:19:22 +0100

    Add files via upload

---
+++ Basic/exercise1.js
@@ -0,0 +1,19 @@
+// Exercise 1: Even or Odd Checker
+let number1 = prompt("Enter a number (Even or Odd Checker)");
+if (number1 % 2 === 0) {
+  alert("The number is even");
+} else {
+  alert("The number is odd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
